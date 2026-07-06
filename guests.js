24db8295fd21798c8e491cc61c1cbafa291238f8
michwolp <michalwolpert@gmail.com>
2026-07-06 23:42:13 +0300

    Update guests.js

--- guests.js
+++ guests.js
@@ -25,7 +25,8 @@ window.GUESTS = {
   DvirSasson:       { name: 'דביר',    fullName: 'דביר ששון',         phone: '0502566643', lang: 'he', form: 'm' },
   DanKedmi:         { name: 'דן',      fullName: 'דן קדמי',           phone: '0509878804', lang: 'he', form: 'm' },
   LiorMandelboim:   { name: 'ליאורי',  fullName: 'ליאור מנדלבוים',     phone: '0546213030', lang: 'he', form: 'f' },
-  RotemAgmon:   { name: 'רתמי',  fullName: 'רתם אגמון',     phone: '0546213030', lang: 'he', form: 'f' },
+  RotemAgmon:   { name: 'רתמי',  fullName: 'רתם אגמון',     phone: '0544317502', lang: 'he', form: 'f' },
+  RonDeitch:   { name: 'רון הבובון',  fullName: 'רון דיטש',     phone: '0547911403', lang: 'he', form: 'm' },
 };
 
 // Look up a guest from the current URL.
